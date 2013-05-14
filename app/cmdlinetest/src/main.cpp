4cdd322d64a285ef5175a171d67237cd8ccd6a8d
Lucien Schreiber <25485796+lucsch@users.noreply.github.com>
2013-05-14 11:14:11 +0200

    Initial code import Adding command line test application

---
+++ app/cmdlinetest/src/main.cpp
@@ -0,0 +1,68 @@
+#include "wx/wxprec.h"
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+#include <wx/app.h>
+#include <wx/cmdline.h>
+#include <wx/dir.h>
+#include <wx/filename.h>
+
+
+static const wxCmdLineEntryDesc cmdLineDesc[] =
+{
+  { wxCMD_LINE_SWITCH, "h", "help", "show this help message", wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_HELP },
+  { wxCMD_LINE_NONE }
+};
+
+
+
+void PrintArray(const wxArrayString & array, const wxString & msg){
+  wxPrintf(msg + _T("\n"));
+  for (unsigned int i = 0; i< array.GetCount(); i++) {
+    wxPrintf(array[i] + _T("\n"));
+  }
+}
+
+
+
+int GetRandomNumber (int maxnumber){
+  int output = 0 + (rand() % (int)(maxnumber - 0 + 1));
+  return output;
+}
+
+
+
+
+
+
+int main(int argc, char **argv)
+{
+  // debugging string for OSX
+  // this is needed for viewing string content with Xcode !!
+  wxString myTest = _T("Test debugging");
+  myTest.ToUTF8().data();
+  myTest.Len();
+
+  wxApp::CheckBuildOptions(WX_BUILD_OPTIONS_SIGNATURE, "program");
+  wxInitializer initializer;
+  if ( !initializer )
+  {
+    fprintf(stderr, "Failed to initialize the wxWidgets library, aborting.");
+    return -1;
+  }
+  wxInitAllImageHandlers();
+
+  wxString myLogoTxt = _T("*\n* drawSpeedTest \n* Testing line drawing speed \n* (c) Copyright 2013 Lucien Schreiber - CREALP . All Rights Reserved. \n*\n");
+  wxCmdLineParser parser(cmdLineDesc, argc, argv);
+  parser.SetLogo(myLogoTxt);
+  if (parser.Parse() != 0) {
+    return 0;
+  }
+
+  wxPrintf(myLogoTxt);
+  return 0;
+}
