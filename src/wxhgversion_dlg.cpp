20ae604c52ce42fa0d82cc5affeb853deb6c0303
Lucien Schreiber <25485796+lucsch@users.noreply.github.com>
2013-05-29 16:17:57 +0200

    Changing author email

--- src/wxhgversion_dlg.cpp
+++ src/wxhgversion_dlg.cpp
@@ -1,18 +1,8 @@
 /***************************************************************************
-				lsversion_dlg.cpp
-                    
-                             -------------------
-    copyright            : (C) 2010 CREALP Lucien Schreiber 
-    email                : lucien.schreiber at crealp dot vs dot ch
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
+ wxhgversion_dlg.cpp
+ -------------------
+ copyright            : (C) 2013  Lucien Schreiber
+ email                : lucien.schreiber at gmail dot com
  ***************************************************************************/
 
 #include "wxhgversion_dlg.h"
