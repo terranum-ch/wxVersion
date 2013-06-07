638ff39577ded9dafa262119f88b54797ef23ebf
?Lucien Schreiber <25485796+lucsch@users.noreply.github.com>
2013-06-07 15:05:45 +0200

    Removing (c) character under Linux, it leads to a crash

--- src/wxhgversion_dlg.cpp
+++ src/wxhgversion_dlg.cpp
@@ -18,12 +18,18 @@ wxDialog(parent, id, title, pos, size,style) {
 	wxString myName = wxString::Format(_("version %s (%s)"),
                                        wxHgVersion::GetSoftNumber(),
                                        wxHgVersion::GetSoftVersion());
+
 	SetSoftName(myName);
+
 	SetModulesName(wxHgVersion::GetAllModuleInfo());
-	wxString myCopy = wxString::Format("\u00A9 Lucien Schreiber, %d",
-									   wxDateTime::Now().GetYear());
+  wxString myCopy=_T("(c)");
+#ifndef __LINUX__
+	myCopy = _T("\u00A9"); // this lead to a crash under linux
+#endif
+	myCopy.Append(wxString::Format(_T(" Lucien Schreiber, %d"), wxDateTime::Now().GetYear()));
 	SetCopyright(myCopy);
     m_ButtonSystemInfoCtrl->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &wxHgVersionDlg::OnButtonSystemInfo, this);
+
 }
 
 
@@ -79,43 +85,43 @@ void wxHgVersionDlg::SetCopyright(wxString name) {
 void wxHgVersionDlg::_CreateControls(){
 	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
 	this->SetFont(wxNullFont);
-	
+
 	wxBoxSizer* bSizer1;
 	bSizer1 = new wxBoxSizer( wxVERTICAL );
-	
+
 	m_BmpCtrl = new wxStaticBitmap( this, wxID_ANY, wxNullBitmap, wxDefaultPosition, wxDefaultSize, 0 );
 	bSizer1->Add( m_BmpCtrl, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5 );
-	
+
 	m_TitleCtrl = new wxStaticText( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
 	m_TitleCtrl->Wrap( -1 );
-		  
+
 	bSizer1->Add( m_TitleCtrl, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5 );
-    
+
 	m_ProgNameCtrl = new wxStaticText( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
 	m_ProgNameCtrl->Wrap( -1 );
 	bSizer1->Add( m_ProgNameCtrl, 0, wxALL|wxALIGN_CENTER_HORIZONTAL, 5 );
-	
+
 	m_ModulesCtrl = new wxTextCtrl( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize(300,180), wxTE_MULTILINE | wxTE_LEFT);
 	bSizer1->Add( m_ModulesCtrl, 1, wxALL|wxEXPAND, 5 );
-	   
+
     wxBoxSizer* bSizer2;
 	bSizer2 = new wxBoxSizer( wxHORIZONTAL );
-	
+
 	m_CopyRightCtrl = new wxStaticText( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
 	m_CopyRightCtrl->Wrap( -1 );
 	bSizer2->Add( m_CopyRightCtrl, 1, wxALL|wxALIGN_LEFT | wxALIGN_CENTER_VERTICAL, 5 );
-	
+
 	m_ButtonSystemInfoCtrl = new wxButton( this, wxID_ANY, wxT("System info"), wxDefaultPosition, wxDefaultSize, 0 );
 	bSizer2->Add( m_ButtonSystemInfoCtrl, 0, wxALL, 5 );
 	bSizer1->Add( bSizer2, 0, wxEXPAND, 5 );
-    
+
 #ifdef __WXOSX__
 	m_TitleCtrl->SetFont(wxNullFont);
 	m_ProgNameCtrl->SetFont(wxNullFont);
 	m_CopyRightCtrl->SetFont(wxNullFont);
 #endif
     m_TitleCtrl->SetFont(m_TitleCtrl->GetFont().MakeBold().MakeLarger());
-	
+
 	this->SetSizer( bSizer1 );
 }
 
